11f6d5fe472999db9ff1e78bbc76467320e0b5b6
Jim Ray <jim@mosaicglobe.com>
2009-02-27 15:19:13 -0700

    update tabs to use resources.

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,11 +1,7 @@
 module Admin::BaseHelper
   
   def tab(label, options = {})
-    if controller.controller_name =~ /#{options[:controller].split('/').last}/
-      content_tag :li, link_to(label, options, {"class"=> "active"}), {"class"=> "active"}
-    else
       content_tag :li, link_to(label, options)
-    end
   end
   
   def render_flash
