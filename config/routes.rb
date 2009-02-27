f8358bbd094fb9367859cd7b26e3139b5a0ed1fb
Jim Ray <jim@mosaicglobe.com>
2009-02-27 15:34:04 -0700

    remove feed

--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/service.wsdl', :action => 'wsdl'
   
   # Install the default route as the lowest priority.
-  #map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id'
 end
