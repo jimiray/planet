99c0fa4ac17b2e6755d502858283ddcf8f5f6d18
Jim Ray <jim@mosaicglobe.com>
2009-02-27 15:17:39 -0700

    fix up for Rails 2.3

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,33 +1,45 @@
-class Admin::UserController < Admin::BaseController
-  
+class Admin::UsersController < Admin::BaseController
+
+  before_filter :find_user, :except => [:index]
+    
   def index
     @users = User.find(:all)
   end
   
   def create
-    @user = User.new(@params['user'])
-    if @request.post? and @user.save
+    @user = User.new(params[:user])
+    if request.post? and @user.save
       flash['notice'] = "User added successfully"
       redirect_to :action=>"index"
     end
   end
   
   def edit
-    @user = User.find(@params['id'])
-    @user.attributes = @params['user']
-    if @request.post? and @user.update
-      flash['notice'] = "User updated successfully"
-      redirect_to :action=>"index"
-    end
     @user.password = @user.password_confirmation = ''
   end
   
+  def update
+    @user.attributes = params[:user]
+    if @user.save
+      flash[:notice] = "User updated"
+      redirect_to admin_users_path
+    else
+      render :action => 'edit'
+    end
+  end
+  
   def delete
-    @user = User.find(@params['id'])
+    @user = User.find(params[:id])
     if @user.destroy
       flash['notice'] = "User deleted successfully"
       redirect_to :action=>"index"
     end
   end
+
+  protected
   
+    def find_user
+      @user = params[:id] ? User.find(params[:id]) : User.new
+    end
+    
 end
