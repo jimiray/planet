48ad7873d399e07ec03dd9e1654e5b8f8ee5ae09
Jim Ray <jim@mosaicglobe.com>
2009-02-27 17:05:03 -0700

    add resources for feeds and posts

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
   map.resources :passwords
   
+  map.resources :tags
+  map.resources :posts
+  map.resources :feeds
+  
   map.namespace :admin do |admin|
     admin.resources :feeds, :collection => {:search => :post}, :member => {:publish => :get, :unpublish => :get}
     admin.resources :users
@@ -15,7 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   # Add your own custom routes here.
   # The priority is based upon order of creation: first created -> highest priority.
   
-  map.connect '', :controller=>"show", :action=>"index"
+  #map.connect '', :controller => "posts", :action=>"index"
   map.admin 'admin', :controller=>"admin/feeds", :action=>"index"
   map.connect 'suggest/', :controller=>"feed", :action=>"suggest"
   map.connect 'contact/', :controller=>"contact", :action=>"index"
@@ -25,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
   # Here's a sample route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
-  map.root :controller => 'show', :action => 'index'
+  map.root :controller => 'posts', :action => 'index'
   
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
